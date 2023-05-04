79673c56bb7adc80c2d1e3e530c6b9429aa76664
Saahil <29069313+SALTx@users.noreply.github.com>
2023-05-04 09:11:07 +0800

    Added js and css routes

--- routes.js
+++ routes.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/js/*", (req, res) => {
+  let path = __dirname + "/views/" + req.url;
+  res.sendFile(path);
+});
+router.get("/css/*", (req, res) => {
+  let path = __dirname + "/views/" + req.url;
+  res.sendFile(path);
+});
+
 router.get("/", (req, res) => {
   res.render("index");
 });
