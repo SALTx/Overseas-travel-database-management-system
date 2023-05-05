fcd96ceae93b51c4d4c4040b0c4718a05a135aa4
Saahil <29069313+SALTx@users.noreply.github.com>
2023-05-05 12:40:28 +0800

    Connected to DB, added student routes

--- routes.js
+++ routes.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "overseas-travel",
+});
 
 router.get("/js/*", (req, res) => {
   let path = __dirname + "/views/" + req.url;
@@ -12,6 +20,57 @@ router.get("/css/*", (req, res) => {
 
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+// Students
+router.get("/students", (req, res) => {
+  let query = "SELECT * FROM students";
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    res.render("students", { students: result });
+  });
+});
+
+router.get("/api/students", (req, res) => {
+  let query = "SELECT * FROM students";
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+router.post("/students", (req, res) => {
+  const data = req.body;
+
+  let adminNo = data.adminNo;
+  let name = data.name;
+  let gender = data.gender;
+  let birthday = data.birthday;
+  let citizenshipStatus = data.citizenshipStatus;
+  let countryOfOrigin = data.countryOfOrigin;
+  let course = data.course;
+  let year = data.year;
+  let pemGroup = data.pemGroup;
+
+  let query = "INSERT INTO students VALUES (?,?,?,?,?,?,?,?,?)";
+  connection.query(
+    query,
+    [
+      adminNo,
+      name,
+      gender,
+      birthday,
+      citizenshipStatus,
+      countryOfOrigin,
+      course,
+      year,
+      pemGroup,
+    ],
+    (err, result) => {
+      if (err) throw err;
+      res.redirect("/students");
+    }
+  );
 });
 
 module.exports = router;
