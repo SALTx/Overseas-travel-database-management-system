31c257ffc5d3dc99ed4e4729afe93eb15c6d601f
Saahil <29069313+SALTx@users.noreply.github.com>
2023-05-09 09:07:37 +0800

    Create getenumvalues.js

---
+++ getenumvalues.js
@@ -0,0 +1,17 @@
+module.exports = function getEnumValues(connection, table, column, callback) {
+  const query = `SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+                   WHERE TABLE_NAME = '${table}'
+                   AND COLUMN_NAME = '${column}'`;
+
+  connection.query(query, (error, results) => {
+    if (error) {
+      callback(error);
+    } else {
+      const values = results[0].COLUMN_TYPE.replace("enum(", "")
+        .replace(")", "")
+        .split(",");
+      const enumValues = values.map((value) => value.replace(/'/g, "").trim());
+      callback(null, enumValues);
+    }
+  });
+};
