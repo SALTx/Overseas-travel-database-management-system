cd783ea4e044993aea8e99326d5db73610c7382a
Saahil <29069313+SALTx@users.noreply.github.com>
2023-05-16 10:26:24 +0800

    Added records routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const routes = require("./routes");
 const studentRoutes = require("./routes/students");
 const programRoutes = require("./routes/programs");
+const recordsRoutes = require("./routes/records");
 const PORT = 3000;
 
 app.set("view engine", "ejs");
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
 app.use("/students", studentRoutes);
 app.use("/programs", programRoutes);
+app.use("/records", recordsRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on port 3000");
