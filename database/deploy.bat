1275ec9e9852eaac52f3057f3b56368b9c0bed4b
Saahil <29069313+SALTx@users.noreply.github.com>
2023-05-19 15:45:36 +0800

    Update deploy.bat

--- database/deploy.bat
+++ database/deploy.bat
@@ -1,12 +1,5 @@
-@REM connect to the mysql database
-mysql.exe -u root
+mysql.exe -u root overseas-travel < deploy.sql
 
-@REM run deploy.sql
-source deploy.sql
 
-@REM run newcountries.sql
-source newcountries.sql
-
-@REM print done and wait for user input
-echo Done!
+@REM pause and wait for user input
 pause
