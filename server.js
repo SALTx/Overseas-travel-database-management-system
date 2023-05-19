5ed92987519f8956e1696e09f9900d061971a1d2
Saahil <29069313+SALTx@users.noreply.github.com>
2023-05-19 09:24:23 +0800

    Added fileupload stuff

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const routes = require("./routes");
 const studentRoutes = require("./routes/students");
 const programRoutes = require("./routes/programs");
 const recordsRoutes = require("./routes/records");
+const importfilesRoutes = require("./routes/importfiles");
+const fileUpload = require("express-fileupload");
 const PORT = 3000;
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 app.use("/", routes);
 app.use("/students", studentRoutes);
 app.use("/programs", programRoutes);
 app.use("/records", recordsRoutes);
+app.use("/functions/import", importfilesRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on port 3000");
