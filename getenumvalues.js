afadd1c297f5eac6cffe569419f246cdc07679fd
Saahil <29069313+SALTx@users.noreply.github.com>
2023-05-19 10:16:52 +0800

    Updated getEnumValues to be async

--- getenumvalues.js
+++ getenumvalues.js
@@ -1,18 +1,19 @@
-module.exports = function getEnumValues(connection, table, column, callback) {
-  const query = `SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS
-                   WHERE TABLE_NAME = '${table}'
-                   AND COLUMN_NAME = '${column}'`;
+module.exports = async function getEnumValues(connection, table, column) {
+  const util = require("util");
+  const queryAsync = util.promisify(connection.query).bind(connection);
 
-  connection.query(query, (error, results) => {
-    if (error) {
-      callback(error, null);
-      return;
-    } else {
-      const values = results[0].COLUMN_TYPE.replace("enum(", "")
-        .replace(")", "")
-        .split(",");
-      const enumValues = values.map((value) => value.replace(/'/g, "").trim());
-      callback(null, enumValues);
-    }
-  });
+  const query = `SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+                 WHERE TABLE_NAME = '${table}'
+                 AND COLUMN_NAME = '${column}'`;
+
+  try {
+    const results = await queryAsync(query);
+    const values = results[0].COLUMN_TYPE.replace("enum(", "")
+      .replace(")", "")
+      .split(",");
+    const enumValues = values.map((value) => value.replace(/'/g, "").trim());
+    return enumValues;
+  } catch (error) {
+    throw error;
+  }
 };
